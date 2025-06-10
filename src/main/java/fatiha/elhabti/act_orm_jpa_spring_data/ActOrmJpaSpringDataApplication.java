142170b0ce1a28e8c97d5d793ecb295adbc674a0
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-06-10 14:26:53 +0100

    creation du projet

---
+++ src/main/java/fatiha/elhabti/act_orm_jpa_spring_data/ActOrmJpaSpringDataApplication.java
@@ -0,0 +1,13 @@
+package fatiha.elhabti.act_orm_jpa_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActOrmJpaSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActOrmJpaSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fatiha/elhabti/act_orm_jpa_spring_data/ActOrmJpaSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package fatiha.elhabti.act_orm_jpa_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActOrmJpaSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
